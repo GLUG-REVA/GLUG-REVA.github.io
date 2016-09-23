1776b9af35920e68f4313ccfd36684b2903e6528
Ujwal P <ujwalp09@gmail.com>
2016-09-23 19:32:31 +0530

    main.js: use https for fetching quotes
    Our website is loaded over https, where as we requested an insecure
    jQuery script as a content which was served over http hence the request
    was blocked.

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($){
 });
 
 // Random Quote Generator
-var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+var url = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var getQuote = function(data) {
   $(".quote-text").text(data.quoteText);
   var quot = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor +' @Rafase282 goo.gl/2h0NHo';
